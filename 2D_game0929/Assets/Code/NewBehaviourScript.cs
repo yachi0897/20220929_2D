b7e300c05ebbbd833291dd1d946c9ab09e0221cd
yachi0897 <113578359+yachi0897@users.noreply.github.com>
2022-10-13 16:05:41 +0800

    20221003_0405
    code

---
+++ 2D_game0929/Assets/Code/NewBehaviourScript.cs
@@ -0,0 +1,41 @@
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+///  <summary>
+///  第一個程式
+///  </summary>
+
+    #region 
+    // 縮排 快捷鍵 Tab
+    // 格式化文件 ctrl +KD
+    // 單行註解
+    /*
+    *
+    *BLABLABLA
+    */
+    #endregion
+
+
+#region  事件區域
+// 喚醒事件:遊戲開始時執行一次
+    private void Awake()
+    {
+
+    }
+// 開始事件 : 在 Awake 後執行一次
+private void Start()
+{
+
+}
+#endregion
+
+
+}
+
+    
