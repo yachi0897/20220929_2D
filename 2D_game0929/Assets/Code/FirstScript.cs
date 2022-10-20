fb8827d0d04ced8f86a42fa63a6e57e59273180c
yachi0897 <113578359+yachi0897@users.noreply.github.com>
2022-10-20 14:18:36 +0800

    20221020
    20221020_01

--- 2D_game0929/Assets/Code/FirstScript.cs
+++ 2D_game0929/Assets/Code/FirstScript.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class NewBehaviourScript : MonoBehaviour
+public class FirstScript : MonoBehaviour
 {
     // Start is called before the first frame update
 ///  <summary>
@@ -26,13 +26,23 @@ public class NewBehaviourScript : MonoBehaviour
 // 喚醒事件:遊戲開始時執行一次
     private void Awake()
     {
-
+     //輸出(訊息);
+     print("Hello,WORLD");
     }
 // 開始事件 : 在 Awake 後執行一次
 private void Start()
 {
-
+     print("<color=yellow>這是開始事件</color>");
 }
+
+//更新事件 : 在 Start 後執行 , 執行次數約 60 FPS
+private void update()
+{
+    print ("<color=red>更新事件 ! </color>");
+}
+
+
+
 #endregion
 
 
---
+++ 2D_game0929/Assets/LearnField.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+namespace Fox
+{
+
+    ///  <summary>
+    ///  學習欄位 Field
+    ///  </summary>
+    public class LearnField : MonoBehaviour
+    {
+
+    }
+
+}
