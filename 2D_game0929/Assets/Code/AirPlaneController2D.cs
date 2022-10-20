f932632df3413892b426b6fcb9883b7b4d8e1cc7
yachi0897 <113578359+yachi0897@users.noreply.github.com>
2022-10-20 16:22:23 +0800

    20221020_02
    20221020_02

---
+++ 2D_game0929/Assets/Code/AirPlaneController2D.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+namespace FOX
+
+{
+    ///<summary>
+    ///飛機控制器 2D 模式
+    ///</summary>
+    public class AirPlaneController2D : MonoBehaviour
+    {
+        [Header("移動速度")]
+        [SerializeField, Range(0, 10)]
+        private float speedVertical = 3.5f;
+        [SerializeField, Range(0, 10)]
+        private float speedHorizontal = 4f;
+        [Header("圖片")]
+        [SerializeField]
+        private Sprite pictureUP;
+        [SerializeField]
+        private Sprite pictureMiddle;
+        [SerializeField]
+        private Sprite pictureDown;
+
+    }
+
+}
---
+++ 2D_game0929/Assets/Code/MoveSystem.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace FOX
+{
+    ///<summary>
+    ///移動系統
+    ///</summary>
+    public class MoveSystem : MonoBehaviour
+    {
+        [SerializeField, Header("移動速度"), Range(-10, 0)]
+        private float speed = -3.5f;
+    }
+
+}
+
