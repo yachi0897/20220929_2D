7323e5998869f2dbe1bcc94c1c4cc3d7a361c1a9
yachi0897 <113578359+yachi0897@users.noreply.github.com>
2022-11-19 00:40:50 +0800

    20221119

--- 2D_game0929/Assets/Code/FireSystemBase.cs
+++ 2D_game0929/Assets/Code/FireSystemBase.cs
@@ -12,15 +12,21 @@ namespace   FOX
         private GameObject prefabBullet;
         [SerializeField, Header("子彈生成點")]
         private Transform pointSpawn;
+        [SerializeField, Header("發射音效")]
+        private AudioClip soundFire;
 
         //自訂方法
         //生成子彈
         //protected 
-           protected void SpawnBullet()
+        protected void SpawnBullet()
         {
             //實例化(物件、座標、角度)
             //生成 子彈預置物 角度與座標跟子彈生成點相同
             Instantiate(prefabBullet, pointSpawn.position, pointSpawn.rotation);
+
+
+            SoundManager.instance.PlaySound(soundFire, new Vector2(0.7f, 1.2f));
+
         }
     }
 }
